423a718b92fcb0518bd67928cdf2149d70f0e29b
Emma <emma.malinsdotter@outlook.com>
2026-03-03 17:22:11 +0100

    förösker publicera

--- config/prod.exs
+++ config/prod.exs
@@ -11,11 +11,13 @@ config :my_app, MyAppWeb.Endpoint, cache_static_manifest: "priv/static/cache_man
 # known as HSTS. If you have a health check endpoint, you may want to exclude it below.
 # Note `:force_ssl` is required to be set at compile-time.
 config :my_app, MyAppWeb.Endpoint,
+  cache_static_manifest: "priv/static/cache_manifest.json",
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   exclude: [
-    # paths: ["/health"],
     hosts: ["localhost", "127.0.0.1"]
-  ]
+  ],
+  server: true
+
 
 # Configure Swoosh API Client
 config :swoosh, api_client: Swoosh.ApiClient.Req
